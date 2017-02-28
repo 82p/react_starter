a5bf0abc1dfd5585dd3941d4bda69793d30d6d9d
wataru haniyama <wataru.haniyama@gmail.com>
2017-02-28 09:25:10 +0900

    add function define

--- src/components/reacttutorial/Square.tsx
+++ src/components/reacttutorial/Square.tsx
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 
 //Squareのプロパティ
 export interface SquareProps extends React.Props<Square> {
-    value: string, onClick: any//どうすりゃいいんだ？ 
+    value: string, onClick: () => void//どうすりゃいいんだ？ 
 };
 //
 export class Square extends React.Component<SquareProps, undefined> {
